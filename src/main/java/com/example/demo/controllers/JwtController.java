0ed8defe44ff9bef497c84a7886111c7307d458f
ib123 <ib123raheem@gmail.com>
2022-04-11 23:52:27 +0300

    project

---
+++ src/main/java/com/example/demo/controllers/JwtController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controllers;
+
+
+import com.example.demo.model.JwtRequest;
+import com.example.demo.model.JwtResponse;
+import com.example.demo.serveces.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+public class JwtController {
+
+    @Autowired
+    private JwtService jwtService;
+
+    @PostMapping({"/authenticate"})
+    public JwtResponse createJwtToken(@RequestBody JwtRequest jwtRequest) throws Exception {
+        return jwtService.createJwtToken(jwtRequest);
+    }
+}
---
+++ src/main/java/com/example/demo/serveces/JwtService.java
@@ -0,0 +1,79 @@
+package com.example.demo.serveces;
+
+
+import com.example.demo.Sequrity.JwtUtil;
+import com.example.demo.model.JwtRequest;
+import com.example.demo.model.JwtResponse;
+import com.example.demo.model.User;
+import com.example.demo.repositries.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class JwtService implements UserDetailsService {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    public JwtResponse createJwtToken(JwtRequest jwtRequest) throws Exception {
+        String userName = jwtRequest.getUserName();
+        String userPassword = jwtRequest.getUserPassword();
+        authenticate(userName, userPassword);
+
+        UserDetails userDetails = loadUserByUsername(userName);
+        String newGeneratedToken = jwtUtil.generateToken(userDetails);
+
+        User user = userRepo.getByUserName(userName).get();
+        return new JwtResponse(user, newGeneratedToken);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.getByUserName(username).get();
+
+        if (user != null) {
+            return new org.springframework.security.core.userdetails.User(
+                    user.getUserName(),
+                    user.getPassword(),
+                    Collections.singleton(getAuthority(user))
+            );
+        } else {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+    }
+
+    private SimpleGrantedAuthority getAuthority(User user) {
+        SimpleGrantedAuthority authorities = new SimpleGrantedAuthority("ROLE_" + user.getRole());
+
+        return authorities;
+    }
+
+    private void authenticate(String userName, String userPassword) throws Exception {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userName, userPassword));
+        } catch (DisabledException e) {
+            throw new Exception("USER_DISABLED", e);
+        } catch (BadCredentialsException e) {
+            throw new Exception("INVALID_CREDENTIALS", e);
+        }
+    }
+}
+
